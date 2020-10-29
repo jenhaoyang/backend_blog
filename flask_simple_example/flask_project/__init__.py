1d80403d3382cf570f0698cda195686110f6b0c2
jenhaoyang <steven05yang@gmail.com>
2020-10-29 04:41:34 +0000

    增加註解

--- flask_simple_example/flask_project/__init__.py
+++ flask_simple_example/flask_project/__init__.py
@@ -11,19 +11,17 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        #非測試的情況下，載入普通設定檔
+        #非測試的情況下，載入production設定檔，包含機敏資料
         app.config.from_pyfile('config.py')
     else:
         #測試情況下載入傳入的測試設定。
         app.config.from_mapping(test_config)
 
-    #確保instance_path存在
+    #確保instance_path存在，因為flask不會自動產生instance資料夾
     try:
         os.mkdir(app.instance_path)
     except OSError:
         pass
-    print(app.instance_path)
-    print(app.config['SECRET_KEY'])
 
     @app.route('/hello')
     def hello():
