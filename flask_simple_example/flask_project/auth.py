781dc3ed12b84998c0a9656d7cb6ac3e6204be08
jenhaoyang <steven05yang@gmail.com>
2020-10-30 07:57:01 +0000

    新增login route

--- flask_simple_example/flask_project/auth.py
+++ flask_simple_example/flask_project/auth.py
@@ -44,3 +44,29 @@ def register():
         flash(error) #flash訊息閃現，訊息不會自己顯示出來，前端必須要用get_flashed_messages把訊息取出，而且會一次取出全部訊息
     
     return render_template('auth/register.html')
+
+
+@bp.route('/login', method=('GET', 'POST'))
+def login():
+    if request.method == 'POST'
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
